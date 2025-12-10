4002a597373ddc34f2561a6fceaa5b97591a37b5
Eduardo <eduardo.rsbtn@eduge.ch>
2025-12-10 14:01:48 +0100

    Ajout du addEventListener pour Enter et logTaskCount pour le nombre de tâches/objets

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
 
+const logTaskCount = function () {
+    console.log("Nombre de tâches :", taskList.children.length);
+}
+
 function addTask() {
 
     const taskText = taskInput.value.trim();
@@ -14,12 +18,26 @@ function addTask() {
     deleteBtn.textContent = 'Suppprimer';
     deleteBtn.addEventListener('click', () => {
         taskList.removeChild(li);
+        logTaskCount()
     });
 
     li.appendChild(deleteBtn);
     taskList.appendChild(li);
 
     taskInput.value = '';
+    logTaskCount()
 }
 
+
 addTaskBtn.addEventListener('click',addTask)
+
+
+taskInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        addTask()
+    }
+});
+
+
+
+
