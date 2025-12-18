64e1ce88b483189b3ecb56d2057a3ad99b0ebcc4
Eduardo <eduardo.rsbtn@eduge.ch>
2025-12-18 11:26:24 +0100

    Suprésion de runAjaxDemo

--- script.js
+++ script.js
@@ -16,14 +16,10 @@ function displayTask(taskText) {
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'Supprimer';
 
-    deleteBtn.addEventListener('click', () => {
+    deleteBtn.addEventListener('click', function() {
+    const index = Array.from(taskList.children).indexOf(li);
 
-        const index = Array.from(taskList.children).indexOf(li);
-
-        fetch(`http://127.0.0.1:5000/tasks/${index}`, {
-
-            method: 'DELETE'
-        })
+    fetch('http://127.0.0.1:5000/tasks/' + index, { method: 'DELETE' })
 
         .then(function(response) {
 
@@ -38,7 +34,7 @@ function displayTask(taskText) {
                 logTaskCount();
             }
         });
-    });
+});
 
     li.appendChild(deleteBtn);
     taskList.appendChild(li);
@@ -103,55 +99,4 @@ taskInput.addEventListener('keydown', function (event) {
         addTask()
     }
 })
-
-function runAjaxDemo() {
-
-    console.log("=== TEST ASYNCHRONE ===");
-
-    let taskCountAsync = 0;
-    console.log("Initialisation (async) :", taskCountAsync);
-
-    const xhrAsync = new XMLHttpRequest();
-    xhrAsync.open("GET", "http://127.0.0.1:5000/tasks", true);
-
-    xhrAsync.onreadystatechange = function () {
-        if (xhrAsync.readyState === 4 && xhrAsync.status === 200) {
-
-            const tasksAsync = JSON.parse(xhrAsync.responseText);
-            taskCountAsync = tasksAsync.length;
-
-            console.log("Réponse reçue (async) :", taskCountAsync);
-
-            fetch("http://127.0.0.1:5000/tasks", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ text: "Tâche ajoutée pendant la démo AJAX" })
-            })
-            .then(() => console.log("➡ Une nouvelle tâche a été ajoutée"));
-
-            console.log("\n=== TEST SYNCHRONE ===");
-
-            let taskCountSync = 0;
-            console.log("Initialisation (sync) :", taskCountSync);
-
-            const xhrSync = new XMLHttpRequest();
-            xhrSync.open("GET", "http://127.0.0.1:5000/tasks", false);
-
-            xhrSync.send();
-
-            if (xhrSync.status === 200) {
-                const tasksSync = JSON.parse(xhrSync.responseText);
-                taskCountSync = tasksSync.length;
-                console.log("Réponse reçue (sync) :", taskCountSync);
-            }
-
-            console.log("Affichage final (sync) :", taskCountSync);
-        }
-    };
-
-    xhrAsync.send();
-
-    console.log("Affichage final (avant réponse async) :", taskCountAsync);
-}
-
 
